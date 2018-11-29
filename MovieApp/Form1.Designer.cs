6b42b63d4d1a9fc3ce38eeae3e90f28fb40cbb95
ianriley97 <ianriley97@ksu.edu>
2018-11-28 19:33:44 -0600

    Added database connection

--- MovieApp/Form1.Designer.cs
+++ MovieApp/Form1.Designer.cs
@@ -28,13 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.queryButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.directorFirstName = new System.Windows.Forms.Label();
+            this.directorLastName = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.actorFirstName = new System.Windows.Forms.Label();
+            this.actorLastName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // queryButton
+            // 
+            this.queryButton.Location = new System.Drawing.Point(325, 257);
+            this.queryButton.Name = "queryButton";
+            this.queryButton.Size = new System.Drawing.Size(280, 117);
+            this.queryButton.TabIndex = 0;
+            this.queryButton.Text = "Look shit up";
+            this.queryButton.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(107, 78);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(149, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(107, 125);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(149, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // directorFirstName
+            // 
+            this.directorFirstName.AutoSize = true;
+            this.directorFirstName.Location = new System.Drawing.Point(129, 60);
+            this.directorFirstName.Name = "directorFirstName";
+            this.directorFirstName.Size = new System.Drawing.Size(113, 15);
+            this.directorFirstName.TabIndex = 3;
+            this.directorFirstName.Text = "Director First Name";
+            // 
+            // directorLastName
+            // 
+            this.directorLastName.AutoSize = true;
+            this.directorLastName.Location = new System.Drawing.Point(129, 107);
+            this.directorLastName.Name = "directorLastName";
+            this.directorLastName.Size = new System.Drawing.Size(113, 15);
+            this.directorLastName.TabIndex = 4;
+            this.directorLastName.Text = "Director Last Name";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(325, 78);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(149, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(325, 125);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(149, 20);
+            this.textBox4.TabIndex = 6;
+            // 
+            // actorFirstName
+            // 
+            this.actorFirstName.AutoSize = true;
+            this.actorFirstName.Location = new System.Drawing.Point(355, 60);
+            this.actorFirstName.Name = "actorFirstName";
+            this.actorFirstName.Size = new System.Drawing.Size(97, 15);
+            this.actorFirstName.TabIndex = 7;
+            this.actorFirstName.Text = "Actor First Name";
+            // 
+            // actorLastName
+            // 
+            this.actorLastName.AutoSize = true;
+            this.actorLastName.Location = new System.Drawing.Point(355, 107);
+            this.actorLastName.Name = "actorLastName";
+            this.actorLastName.Size = new System.Drawing.Size(97, 15);
+            this.actorLastName.TabIndex = 8;
+            this.actorLastName.Text = "Actor Last Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1014, 450);
+            this.Controls.Add(this.actorLastName);
+            this.Controls.Add(this.actorFirstName);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.directorLastName);
+            this.Controls.Add(this.directorFirstName);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.queryButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button queryButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label directorFirstName;
+        private System.Windows.Forms.Label directorLastName;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label actorFirstName;
+        private System.Windows.Forms.Label actorLastName;
     }
 }
 
--- MovieApp/Form1.cs
+++ MovieApp/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Data.SqlClient;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,18 @@ namespace MovieApp
 {
     public partial class Form1 : Form
     {
+        private static string databaseConnectionString;
+
+
+
         public Form1()
         {
             InitializeComponent();
+            SqlConnection connect = new SqlConnection();
+            connect.ConnectionString = "Data Source = mssql.cs.ksu.edu;" +
+                "Initial Catalog=cis560_team16;" +
+                "Integrated Security=SSPI;";
+            connect.Open();
         }
     }
 }
